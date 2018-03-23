ae0341588fcd40e5e28a4c2c5c48a4e559cd04ea
VVGLML <atmismahir@gmail.com>
2018-03-23 05:50:09 +0300

    CNN With CheckPoints And TensorBoardX

--- 10_1_cnn_mnist.py
+++ 10_1_cnn_mnist.py
@@ -1,23 +1,41 @@
 # https://github.com/pytorch/examples/blob/master/mnist/main.py
 from __future__ import print_function
-import argparse
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.autograd import Variable
+import os.path
+import time
+from ozu_config import settings as cfg
+from tensorboardX import SummaryWriter
+from torchviz import make_dot
+
 
 # Training settings
 batch_size = 64
+num_epochs = 10000
+print_every = 100
+start_epoch = 0
+best_accuracy = torch.FloatTensor([0])
+
+# CUDA?
+cuda = torch.cuda.is_available()
+
+# Seed for reproducibility
+torch.manual_seed(1)
+if cuda:
+    torch.cuda.manual_seed(1)
+
 
 # MNIST Dataset
-train_dataset = datasets.MNIST(root='./data/',
+train_dataset = datasets.MNIST(root=cfg['DATASETS_DIR']+'mnist/',
                                train=True,
                                transform=transforms.ToTensor(),
                                download=True)
 
-test_dataset = datasets.MNIST(root='./data/',
+test_dataset = datasets.MNIST(root=cfg['DATASETS_DIR']+'mnist/',
                               train=False,
                               transform=transforms.ToTensor())
 
@@ -51,43 +69,151 @@ class Net(nn.Module):
 
 model = Net()
 
-optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+# If you are running a GPU instance, load the model on GPU
+if cuda:
+    model.cuda()
+
+# #### Loss and Optimizer ####
+# Softmax is internally computed.
+loss_fn = nn.CrossEntropyLoss()
+# If you are running a GPU instance, compute the loss on GPU
+if cuda:
+    loss_fn.cuda()
+
+# Set parameters to be updated.
+optimizer = optim.Adam(model.parameters(), lr=0.01)
+model_pth = cfg['DATASETS_DIR'] + 'output/checkpoint2.pth.tar'
+# If exists a best model, load its weights!
+if os.path.isfile(model_pth):
+    print("=> loading checkpoint '{}' ...".format(model_pth))
+    if cuda:
+        checkpoint = torch.load(model_pth)
+    else:
+        # Load GPU model on CPU
+        checkpoint = torch.load(model_pth,
+                                map_location=lambda storage,
+                                loc: storage)
+    start_epoch = checkpoint['epoch']
+    best_accuracy = checkpoint['best_accuracy']
+    model.load_state_dict(checkpoint['state_dict'])
+    print("=> loaded checkpoint '{}' (trained for {} epochs)".format(
+        model_pth,
+        checkpoint['epoch']))
 
 
 def train(epoch):
+    """Perform a full training over dataset"""
+    average_time = 0
+    # Model train mode
     model.train()
-    for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = Variable(data), Variable(target)
+    for i, (images, labels) in enumerate(train_loader):
+        # measure data loading time
+        batch_time = time.time()
+        images = Variable(images)
+        labels = Variable(labels)
+
+        if cuda:
+            images, labels = images.cuda(), labels.cuda()
+
+        # Forward + Backward + Optimize
         optimizer.zero_grad()
-        output = model(data)
-        loss = F.nll_loss(output, target)
+        outputs = model(images)
+        if epoch == 1:
+            # visualize execution graph
+            make_dot(outputs, params=dict(model.named_parameters()))
+        loss = loss_fn(outputs, labels)
+
+        # Load loss on CPU
+        if cuda:
+            loss.cpu()
+
         loss.backward()
         optimizer.step()
-        if batch_idx % 10 == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.data[0]))
+
+        # Measure elapsed time
+        batch_time = time.time() - batch_time
+        # Accumulate over batch
+        average_time += batch_time
+
+        # ### Keep track of metric every batch
+        # Accuracy Metric
+        prediction = outputs.data.max(1)[1]   # first column has actual prob.
+        accuracy = 100. * prediction.eq(labels.data).sum() / batch_size
+
+        # Log
+        if (i + 1) % print_every == 0:
+            print (('Epoch:{}/{}, Step:{}/{}, Loss:{:.4f}, Accur: {:.4f}' +
+                    ', Batch time: {:.4f}').format(
+                epoch + 1,
+                num_epochs,
+                i + 1,
+                len(train_dataset) / batch_size,
+                loss.data[0],
+                accuracy,
+                average_time/print_every))  # Average
 
 
-def test():
+def eval(model):
+    """Eval over test set"""
     model.eval()
-    test_loss = 0
     correct = 0
+    # Get Batch
     for data, target in test_loader:
         data, target = Variable(data, volatile=True), Variable(target)
+        if cuda:
+            data, target = data.cuda(), target.cuda()
+        # Evaluate
         output = model(data)
-        # sum up batch loss
-        test_loss += F.nll_loss(output, target, size_average=False).data[0]
-        # get the index of the max log-probability
-        pred = output.data.max(1, keepdim=True)[1]
-        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
-
-    test_loss /= len(test_loader.dataset)
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
+        # Load output on CPU
+        if cuda:
+            output.cpu()
+        # Compute Accuracy
+        prediction = output.data.max(1)[1]
+        correct += prediction.eq(target.data).sum()
+    return correct
 
 
-for epoch in range(1, 10):
+def save_checkpoint(state, is_best, filename='/output/checkpoint.pth.tar'):
+    """Save checkpoint if a new best is achieved"""
+    if is_best:
+        print ("=> Saving a new best")
+        if not os.path.exists(filename):
+            with open(filename, 'w'):
+                pass
+        torch.save(state, filename)  # save checkpoint
+    else:
+        print ("=> Validation Accuracy did not improve")
+
+
+dataset_length = len(test_loader.dataset)
+
+# to visualize using tensorboard
+# to viualize the logs run the following on a terminal
+# tensorboard --log_dir=/home/vvglab/tblogs port=6006
+# tensorboard --logdir=cfg['TENSORBOARD_LOGDIR'] port=6006
+# open a browser and type http://localhost:6006
+
+writer = SummaryWriter(log_dir=cfg['TENSORBOARD_LOGDIR'])
+
+for epoch in range(start_epoch, num_epochs):
     train(epoch)
-    test()
+    best_accuracy
+    acc = eval(model)
+    acc = 100. * acc / dataset_length
+    print('=> Test set: Accuracy: {:.2f}%'.format(acc))
+    acc = torch.FloatTensor([acc])
+    # Get bool not ByteTensor
+    is_best = bool(acc.numpy() > best_accuracy.numpy())
+    # Get greater Tensor to keep track best acc
+    best_accuracy = torch.FloatTensor(max(acc.numpy(), best_accuracy.numpy()))
+    # Save checkpoint if is a new best
+    fname = cfg['DATASETS_DIR']+'output/checkpoint'+str(epoch+1)+'.pth.tar'
+    save_checkpoint({
+        'epoch': epoch + 1,
+        'state_dict': model.state_dict(),
+        'best_accuracy': best_accuracy
+    }, is_best, filename=fname)
+    writer.add_scalar('data/Accuracy', acc, epoch)
+    writer.add_scalar('data/BestAcc', best_accuracy, epoch)
+
+writer.close()
--- ozu_config.py
+++ ozu_config.py
@@ -2,7 +2,10 @@
 from getpass import getuser
 
 settings = {}
-ozu_log_dir = ("/mnt/09434021-6a4d-4335-8aca-ce25516d62c7/"
-               "SHARE/tensorboard_log/zeroToAll")
+
+shared_folder = "/mnt/09434021-6a4d-4335-8aca-ce25516d62c7/SHARE/"
+ozu_log_dir = (shared_folder + "tensorboard_log/zeroToAll")
 log_dir = '/home/vvglab/tblogs' if getuser() == 'vvglab' else ozu_log_dir
+
 settings['TENSORBOARD_LOGDIR'] = log_dir
+settings['DATASETS_DIR'] = shared_folder+"Datasets/"
